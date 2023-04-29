e72cebbb43f3c9ee5ef8a8564c889fd8b35c5332
JuanGachi <jugach21@gmail.com>
2023-04-29 20:49:07 +0200

    proyectopascua

--- Ventanas/ventanaprincipal.py
+++ Ventanas/ventanaprincipal.py
@@ -1,13 +1,15 @@
+import os
+import subprocess
 from PyQt5 import QtWidgets, QtGui
 from PyQt5.QtCore import Qt, QPoint
-from ventanaregistro import RegistroVentana
+from ventanaregistro import RegistroVentana  # Asegúrate de que esta línea esté presente
 
 import sys
 sys.path.append(r'C:\Users\PC\Desktop\Py\Ventanas')
 
-class RegistroVentana(QtWidgets.QWidget):
+class VentanaPrincipal(QtWidgets.QWidget):
     def __init__(self):
-        super(RegistroVentana, self).__init__()
+        super(VentanaPrincipal, self).__init__()
 
         # Configurar la ventana
         self.setWindowTitle('Aplicación Móvil')
@@ -47,6 +49,14 @@ class RegistroVentana(QtWidgets.QWidget):
         boton_volver.setGeometry(10, 10, 70, 30)
         boton_volver.setStyleSheet("background-color: red; color: white;")
         boton_volver.clicked.connect(self.mostrar_ventana_principal)
+        
+        # Crear el botón "Buscar"
+        self.boton_buscar = QtWidgets.QPushButton('Buscar', self)
+        self.boton_buscar.setGeometry(30, 390, 100, 100)  # Establecer posición y tamaño
+        self.boton_buscar.setStyleSheet("border-radius: 50px; background-color: green; color: white;")  # Establecer estilo de botón redondeado y verde
+        self.boton_buscar.clicked.connect(self.ejecutar_boton_reconocimiento)
+        self.boton_buscar.setEnabled(False)  # Deshabilitar el botón "Buscar" al inicio
+        
     def mousePressEvent(self, event):
         self.oldPos = event.globalPos()
 
@@ -56,21 +66,28 @@ class RegistroVentana(QtWidgets.QWidget):
         self.oldPos = event.globalPos()
 
     def abrir_ventana_registro(self):
-        print("es ok")
         self.hide()  # Ocultar la ventana principal
-        ventana_registro = RegistroVentana()
-        print(ventana_registro)
-        ventana_registro.show()  # Mostrar la ventana de registro
-        app.exec_()
-
+        self.ventana_registro = RegistroVentana()  # Crear una instancia de RegistroVentana
+        self.ventana_registro.show()  # Mostrar la ventana de registro
+        self.ventana_registro.setWindowTitle("Registro de Usuarios")
+        self.ventana_registro.setFixedSize(400, 500)
+        self.ventana_registro.boton_registro.clicked.connect(self.mostrar_ventana_principal)
+        self.ventana_registro.boton_registro.clicked.connect(self.usuario_registrado)
+    def usuario_registrado(self):
+        self.ventana_registro.registrar_usuario()
+        self.boton_buscar.setEnabled(True)  # Habilitar el botón "Buscar" después de que el usuario se haya registrado
+        self.mostrar_ventana_principal()
     def mostrar_ventana_principal(self):
-        self.hide()  # Ocultar la ventana de registro
+        self.ventana_registro.hide()  # Ocultar la ventana de registro
         self.show()  # Mostrar la ventana principal
+    def ejecutar_boton_reconocimiento(self):
+        script_path = os.path.join(os.getcwd(), "botonreconocimiento3.py")
+        subprocess.run(["python", script_path])  # Ejecutar el script "botonreconocimiento3.py"
 
 
 if __name__ == '__main__':
     app = QtWidgets.QApplication([])
-    ventana = RegistroVentana()
+    ventana = VentanaPrincipal()
 
     # Centrar ventana en pantalla
     screen = QtWidgets.QDesktopWidget().screenGeometry()
